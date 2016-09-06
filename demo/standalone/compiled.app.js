99d017c57833ecab7fb4864a026ebb1011217bea
Misha Moroshko <michael.moroshko@gmail.com>
2016-09-06 21:39:02 +1000

    Add standalone lib and demo and upgrade deps

---
+++ demo/standalone/compiled.app.js
@@ -0,0 +1,130 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(1);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	/* eslint-disable react/react-in-jsx-scope */
+
+	var items = [{
+	  text: 'Apple'
+	}, {
+	  text: 'Banana'
+	}, {
+	  text: 'Cherry'
+	}, {
+	  text: 'Grapefruit'
+	}, {
+	  text: 'Lemon'
+	}];
+
+	var renderItem = function renderItem(item) {
+	  return item.text;
+	};
+
+	var App = function (_React$Component) {
+	  _inherits(App, _React$Component);
+
+	  // eslint-disable-line no-undef
+	  function App() {
+	    _classCallCheck(this, App);
+
+	    var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this));
+
+	    _this.onChange = function (event, _ref) {
+	      var newValue = _ref.newValue;
+
+	      _this.setState({
+	        value: newValue
+	      });
+	    };
+
+	    _this.state = {
+	      value: ''
+	    };
+	    return _this;
+	  }
+
+	  _createClass(App, [{
+	    key: 'render',
+	    value: function render() {
+	      var value = this.state.value;
+
+	      var inputProps = {
+	        placeholder: 'Type to filter fruits',
+	        value: value,
+	        onChange: this.onChange
+	      };
+
+	      return React.createElement(Autowhatever // eslint-disable-line react/jsx-no-undef
+	      , { items: items,
+	        renderItem: renderItem,
+	        inputProps: inputProps,
+	        focusedItemIndex: 2 });
+	    }
+	  }]);
+
+	  return App;
+	}(React.Component);
+
+	ReactDOM.render(React.createElement(App, null), document.getElementById('app')); // eslint-disable-line no-undef
+
+/***/ }
+/******/ ]);
